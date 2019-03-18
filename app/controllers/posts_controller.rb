0213862994ff46eecbf6efee285be303edec2e98
Boots2291 <buteramv@gmail.com>
2019-03-18 09:57:07 -0400

    Add new and create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,17 @@ class PostsController < ApplicationController
   def index
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
   private
     def post_params
-      params.require(:post).permit(:caption, :image)
+      params.require(:post).permit(:image, :caption)
     end
 end
