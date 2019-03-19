e10def0867852c9b632678197a7ff0838534c125
Boots2291 <buteramv@gmail.com>
2019-03-19 12:31:18 -0400

    Add post timer

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :comments, dependent: :destroy
+
   validates :user_id, presence: true
+  validates :caption, presence: true, length: { minimum: 3, maximum: 300 }
 end
